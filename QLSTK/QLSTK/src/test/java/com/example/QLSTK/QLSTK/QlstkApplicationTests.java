fb341e9e686cb9d33d25eac38548d73dac89bb92
@Hqs2702 <quocsang05.uit@gmail.com>
2025-03-30 23:51:44 +0700

    crate project

---
+++ QLSTK/QLSTK/src/test/java/com/example/QLSTK/QLSTK/QlstkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QLSTK.QLSTK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlstkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
