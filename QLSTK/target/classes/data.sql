a60f2843100c5050aa9d1093412f4a3d01aa2860
hathuanihi <hathu20905@gmail.com>
2025-05-04 01:11:31 +0700

    basic api for programming, include gg sign in

--- QLSTK/target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO SOTIETKIEM (maSTK, tenSTK, soTienGuiToiThieu, kyHan, laiSuat, thoiGianGuiToiThieu, tenKH, diaChi)
-VALUES
-(1, 'Không kỳ hạn', 1000000, 0, 0.005, 15, 'Khách hàng', 'Địa chỉ'),
-(2, '3 tháng', 1000000, 3, 0.05, 90, 'Khách hàng', 'Địa chỉ'),
-(3, '6 tháng', 1000000, 6, 0.055, 180, 'Khách hàng', 'Địa chỉ');
